36c60ef5a2810cfc50f2a0a52da9b1a9b494c7da
Bibilna Babu <bibilnababu@yahoo.com>
2023-03-10 12:18:15 +0000

    refactor promise handling with async/await

--- exercise001.js
+++ exercise001.js
@@ -1,21 +1,36 @@
 const promise = new Promise((resolve, reject) => {
-	// Generate random number between 0 and 9
-	const randomInt = Math.floor(Math.random() * 10);
+  // Generate random number between 0 and 9
+  const randomInt = Math.floor(Math.random() * 10);
 
-	if (randomInt % 2 === 0) {
-		// Success
-		setTimeout(() => resolve('done'), 2000);
-	} else {
-		// Failure
-		setTimeout(() => reject('error'), 2000);
-	}
+  if (randomInt % 2 === 0) {
+    // Success
+    setTimeout(() => resolve("done"), 2000);
+  } else {
+    // Failure
+    setTimeout(() => reject("error"), 2000);
+  }
 });
 
 // Your solution(s) to exercise001 here!
-promise
-.then((response)=>{
-console.log(`Yay! Promise resolved with response: ${response}`);
-})
-.catch((response)=>{
-	console.log(`Boo. Promise rejected with response:${response} `);
-});
+// promise
+//   .then((response) => {
+//     console.log(`Yay! Promise resolved with response: ${response}`);
+//   })
+//   .catch((response) => {
+//     console.log(`Boo. Promise rejected with response:${response} `);
+//   });
+
+
+//modifying into async and await
+// const result = await somePromiseObject;
+const getPromiseResponse = async () => {
+	/*  your function here */
+	try{
+		const response = await promise;
+		console.log(`Yay! Promise resolved with response: ${response}`);
+	}catch(error){
+		console.log(`Boo. Promise rejected with response:${error}`);
+	}
+
+}
+getPromiseResponse();
